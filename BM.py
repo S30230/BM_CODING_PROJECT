ea563ee8f11cfab56927834d8b734a0d3d25e5fc
S30230 <168240178+S30230@users.noreply.github.com>
2024-05-17 16:34:59 -0700

    Update BM.py

--- BM.py
+++ BM.py
@@ -1,17 +1,19 @@
 def main():
-    print("                                                          Welcome to Trivia!")
-    player_name = input("Enter your name: ")
+    print("                                                          Welcome to the challenge where you fail, you die!")
+    player_name = input("Enter your name... or die: ")
     print(f"                                                         Hello, {player_name}! Let's start.")
     
     # Starting point of the game
-    print("                                                          The difficulty of this game is set to hard, do you wish to continue?")
+    print("                                                          There are two parts to this challenge, the is the first part, trivia! Do you wish to continue?")
+    
     first_choice = input("Yes or No? (Yes/No): ")
     
     
     if first_choice.lower() == "yes":
         hehe()
     else:
-        print("                                                      Aww, guess I'll see you next time.")
+        print("                                                      Aww, guess I'll see you next time... ")
+        print("                                                      Or is there a next time?")
         return
 
 
@@ -25,12 +27,17 @@ def hehe():
     elif riddle_answer.lower() == "keyboard":
         print("                                                      Correct! Wait, is this too easy for you?")
     else:
-        print("                                                      Wrong answer. Better luck next time.")
+        print("                                                      Wrong answer. Better luck next time... Corpse!")
+        return
     print("                                                          Guess I really need to come up with some harder ones... Anyways, second question..")
     riddle_answer = input("                                          I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I? ")
     if riddle_answer.lower() == "echo":
         print("                                                      Really bro? Imma die if u get one more.")  
     else:
-        print("                                                      Wrong answer. Better luck next time.")
+        print("                                                      Wrong answer. Better luck next time.... Hehe")
+        return
+    print("Third question")
+    print("Actually, u pass! Time to go to the next part of the challenge, its a gimkit!")
+    
 if __name__ == "__main__":
     main()
